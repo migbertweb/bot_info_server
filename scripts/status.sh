0fa952fd8159381beba618984950be83818c6f17
migbertweb <migbertyanez@disroot.org>
2025-02-08 16:33:41 -0300

    status

--- scripts/status.sh
+++ scripts/status.sh
@@ -19,15 +19,3 @@ kernel_version=$(uname -r)
 top_processes=$(ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 6 | awk '{print $1, $2, $3, $4"%", $5"%"}')
 # Crear mensaje
 echo -e "🖥️ **Estado del Sistema**\n- CPU: $cpu_usage%\n- Memoria: $mem_usage\n- Versión del kernel: $kernel_version\n- Disco: $disk_usage\n- Carga del sistema: $load_avg\n- Uptime: $uptime\n- Usuarios conectados: $users_connected\n- Red: RX $network_rx_mb MB / TX $network_tx_mb MB\n- Temperatura CPU: $cpu_temp\n- Procesos en ejecución: $processes\n- $top_processes"
-
-##!/bin/sh
-#set -e  # Detener el script si hay un error
-#
-## Obtener métricas del sistema
-#cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
-#mem_usage=$(free -m | awk 'NR==2{printf "%.2f%%", $3*100/$2}')
-#disk_usage=$(df -h / | awk 'NR==2{print $5}')
-#
-## Crear mensaje
-#echo -e "🖥️ **Estado del VPS**\n- CPU: $cpu_usage%\n- Memoria: $mem_usage\n- Disco: $disk_usage"
-#
